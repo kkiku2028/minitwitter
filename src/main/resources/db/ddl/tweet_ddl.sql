7ec807296f91d183fe39e77467262cb1bf2bfee7
koichi <koichi@p1t6rpeg>
2022-07-17 15:20:28 +0900

    DDLフォルダ追加

--- src/main/resources/db/ddl/tweet_ddl.sql
+++ src/main/resources/db/ddl/tweet_ddl.sql
@@ -1,6 +1,6 @@
 create table tweet (
    id integer primary key auto_increment,
-   user_id integer,
+   user_id integer not null,
    content varchar(1000) not null,
    create_time timestamp default current_timestamp,
    update_time timestamp default current_timestamp on update current_timestamp,
