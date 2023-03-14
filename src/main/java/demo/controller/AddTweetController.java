46a71f46465efba456e621e34a488e3ded1b8c50
takufon <deuberg962@outlook.jp>
2023-03-14 23:47:58 +0900

    実装途中のファイル処理を削除・pomを修正

--- src/main/java/demo/controller/AddTweetController.java
+++ src/main/java/demo/controller/AddTweetController.java
@@ -1,22 +1,12 @@
 package demo.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Base64;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
-import demo.entity.TweetEntity;
 import demo.form.TweetForm;
 import demo.service.AddTweetService;
 
@@ -35,16 +25,8 @@ public class AddTweetController {
 	}
 	
 	@PostMapping
-	public String create(@RequestParam("file") MultipartFile file, TweetForm form) throws IOException {
+	public String create(TweetForm form) {
 
-		TweetEntity savedFile = addTweetService.store(file, form);
-		byte[] bytes = savedFile.getData();
-		// ポイント4: Base64.getEncoder().encodeToString(bytes)でbyteをStringにして、Viewに渡す
-
-//		//登録された画像データからファイル名を取得
-//		String fileName = form.getMultipartFile().getOriginalFilename();
-//		String str = form.getStr();
-//		File filePath = new File(tweetImg.getOriginalFilename());
 		addTweetService.save(form);
 		return "redirect:/minitwitter/tweet-list";
 	}
--- src/main/java/demo/service/AddTweetService.java
+++ src/main/java/demo/service/AddTweetService.java
@@ -1,11 +1,8 @@
 package demo.service;
 
-import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import demo.dao.TweetDao;
 import demo.entity.TweetEntity;
@@ -15,14 +12,6 @@ import demo.form.TweetForm;
 public class AddTweetService {
 	@Autowired
 	TweetDao tweetDao;
-
-	public TweetEntity store(MultipartFile file, TweetForm tweetForm) throws IOException {
-		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-		TweetEntity tweetEntity = new TweetEntity(fileName, file.getContentType(), file.getBytes());
-		tweetEntity.setContent(tweetForm.getContent());
-		tweetEntity.setUser_id(tweetForm.getUser_id());
-		return tweetDao.save(tweetEntity);
-	}
 	
 	public void save(TweetForm tweetForm) {
 		TweetEntity tweet = new TweetEntity();
